d4f80de36b4079a55f1fca54d4795a188917f66b
KostBoh <bohdan.kostya@gmail.com>
2024-07-27 23:31:08 +0300

    add change updateContact

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -4,11 +4,6 @@ import ctrlWrapper from "../decorators/ctrlWrapper.js";
 
 import HttpError from "../helpers/HttpError.js";
 
-// import {
-//   createContactSchema,
-//   updateContactSchema,
-// } from "../schemas/contactsSchemas.js";
-
 const getAllContacts = async (req, res) => {
   const result = await contactsService.listContacts();
   res.json(result);
@@ -35,21 +30,19 @@ const deleteContact = async (req, res) => {
 };
 
 const createContact = async (req, res) => {
-  //   const { error } = createContactSchema.validate(req.body);
-  //   if (error) {
-  //     throw HttpError(400, error.message);
-  //   }
   const result = await contactsService.addContact(req.body);
   res.status(201).json(result);
 };
 
 const updateContact = async (req, res) => {
-  //   const { error } = updateContactSchema.validate(req.body);
-  //   if (error) {
-  //     throw HttpError(400, error.message);
-  //   }
   const { id } = req.params;
-  const result = await contactsService.updateById(id, req.body);
+  const body = req.body;
+
+  if (Object.keys(body).length === 0) {
+    throw HttpError(400, "Body must have at least one field");
+  }
+
+  const result = await contactsService.updateById(id, body);
   if (!result) {
     throw HttpError(404, `Not found`);
   }
