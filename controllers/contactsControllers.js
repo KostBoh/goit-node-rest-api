c1a6e83598bda82de635bd9de47bd309705e42a0
KostBoh <bohdan.kostya@gmail.com>
2024-07-27 20:35:53 +0300

    add change validate schema

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -4,10 +4,10 @@ import ctrlWrapper from "../decorators/ctrlWrapper.js";
 
 import HttpError from "../helpers/HttpError.js";
 
-import {
-  createContactSchema,
-  updateContactSchema,
-} from "../schemas/contactsSchemas.js";
+// import {
+//   createContactSchema,
+//   updateContactSchema,
+// } from "../schemas/contactsSchemas.js";
 
 const getAllContacts = async (req, res) => {
   const result = await contactsService.listContacts();
@@ -35,19 +35,19 @@ const deleteContact = async (req, res) => {
 };
 
 const createContact = async (req, res) => {
-  const { error } = createContactSchema.validate(req.body);
-  if (error) {
-    throw HttpError(400, error.message);
-  }
+  //   const { error } = createContactSchema.validate(req.body);
+  //   if (error) {
+  //     throw HttpError(400, error.message);
+  //   }
   const result = await contactsService.addContact(req.body);
   res.status(201).json(result);
 };
 
 const updateContact = async (req, res) => {
-  const { error } = updateContactSchema.validate(req.body);
-  if (error) {
-    throw HttpError(400, error.message);
-  }
+  //   const { error } = updateContactSchema.validate(req.body);
+  //   if (error) {
+  //     throw HttpError(400, error.message);
+  //   }
   const { id } = req.params;
   const result = await contactsService.updateById(id, req.body);
   if (!result) {
--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -2,6 +2,16 @@ import express from "express";
 
 import contactsControllers from "../controllers/contactsControllers.js";
 
+import validateBody from "../helpers/validateBody.js";
+import {
+  createContactSchema,
+  updateContactSchema,
+} from "../schemas/contactsSchemas.js";
+
+const createContactMiddleware = validateBody(createContactSchema);
+
+const updateContactMiddleware = validateBody(updateContactSchema);
+
 const contactsRouter = express.Router();
 
 contactsRouter.get("/", contactsControllers.getAllContacts);
@@ -10,8 +20,16 @@ contactsRouter.get("/:id", contactsControllers.getOneContact);
 
 contactsRouter.delete("/:id", contactsControllers.deleteContact);
 
-contactsRouter.post("/", contactsControllers.createContact);
+contactsRouter.post(
+  "/",
+  createContactMiddleware,
+  contactsControllers.createContact
+);
 
-contactsRouter.put("/:id", contactsControllers.updateContact);
+contactsRouter.put(
+  "/:id",
+  updateContactMiddleware,
+  contactsControllers.updateContact
+);
 
 export default contactsRouter;
