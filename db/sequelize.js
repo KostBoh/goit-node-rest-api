a4f911150015972e1f06981772f7948a95d45f02
KostBoh <bohdan.kostya@gmail.com>
2024-08-03 00:30:57 +0300

    add change gitignore

--- db/sequelize.js
+++ db/sequelize.js
@@ -1,15 +1,6 @@
 import { Sequelize } from "sequelize";
+import { databaseConfig } from "./config.js";
 
-const sequelize = new Sequelize({
-  dialect: "postgres",
-  username: "kost",
-  database: "db_contacts_t2te",
-  password: "7rZsOJdfHe9JScvCtYRSWiA6qYf4Pf1z",
-  host: "dpg-cqmi2eggph6c739qsii0-a.frankfurt-postgres.render.com",
-  port: "5432",
-  dialectOptions: {
-    ssl: true,
-  },
-});
+const sequelize = new Sequelize(databaseConfig);
 
 export default sequelize;
