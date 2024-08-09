b790d0e38e9cb2afe073b855945f1d03c008dd11
KostBoh <bohdan.kostya@gmail.com>
2024-08-09 22:20:00 +0300

    add first step and create files

---
+++ routes/authRouter.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+
+import authControllers from "../controllers/authControllers.js";
+
+import validateBody from "../helpers/validateBody.js";
+
+import { authSignupSchema } from "../schemas/authSchemas.js";
+
+const signupMiddleware = validateBody(authSignupSchema);
+
+const authRouter = Router();
+
+authRouter.post("/signup", signupMiddleware, authControllers.signup);
+
+export default authRouter;
