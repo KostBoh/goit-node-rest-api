b5278e8768cf547055065b07b03d6885a82fd267
KostBoh <bohdan.kostya@gmail.com>
2024-08-10 23:10:28 +0300

    add change second part

--- services/authServices.js
+++ services/authServices.js
@@ -7,6 +7,25 @@ export const findUser = (query) =>
     where: query,
   });
 
+export const updateUser = async (query, data) => {
+  const user = await findUser(query);
+  if (!user) {
+    return null;
+  }
+  return user.update(data, {
+    returning: true,
+  });
+  // const [update] = await Contact.update(data, {
+  //   where: query,
+  //   returning: true,
+  // });
+  // if (update) {
+  //   const updateContact = await Contact.findOne({ where: query });
+  //   return updateContact;
+  // }
+  // throw HttpError(404, "Not found");
+};
+
 export const signup = async (data) => {
   try {
     const { password } = data;
