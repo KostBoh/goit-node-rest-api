ea12f9753df7ff70d6f0c11da091fe1fb958ad65
KostBoh <bohdan.kostya@gmail.com>
2024-08-11 10:09:40 +0300

    add change name routs

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -1,7 +1,8 @@
 import Contact from "../db/models/Contact.js";
 import HttpError from "../helpers/HttpError.js";
 
-export const listContacts = (query = {}, { page = 1, limit = 10 }) => {
+export const listContacts = (query = {}, options = {}) => {
+  const { page = 1, limit = 10 } = options;
   const normalizedLimit = Number(limit);
   const offset = (Number(page) - 1) * normalizedLimit;
   return Contact.findAll({
