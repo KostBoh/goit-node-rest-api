d14ebb321ff17f237bf50847c8760df7016a7b7c
KostBoh <bohdan.kostya@gmail.com>
2024-08-11 10:26:40 +0300

    add change logout

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -14,16 +14,10 @@ const authRouter = Router();
 
 authRouter.post("/register", authControllers.register);
 
-// authRouter.post("/signup", authControllers.signup);
-
 authRouter.post("/login", registerMiddleware, authControllers.login);
-
-// authRouter.post("/signin", registerMiddleware, authControllers.signin);
 
 authRouter.get("/current", authenticate, authControllers.getCurrent);
 
 authRouter.post("/logout", authenticate, authControllers.logout);
 
 export default authRouter;
-
-// signupMiddleware
