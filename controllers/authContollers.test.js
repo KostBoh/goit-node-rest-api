56fb4a36bf2743d2d63940c13b1d03b40ad4c9eb
KostBoh <bohdan.kostya@gmail.com>
2024-08-21 21:18:29 +0300

    add change start test

--- controllers/authContollers.test.js
+++ controllers/authContollers.test.js
@@ -1,3 +1,27 @@
+import request from "supertest";
+
 import app from "../app.js";
 
-describe("test / Login Controller", () => {});
+describe("test / Login Controller", () => {
+  beforeAll(() => {});
+
+  afterAll(() => {
+    app.close();
+  });
+
+  afterEach(async () => {});
+
+  test("login with correct data", async () => {
+    const loginData = {
+      email: "iii@gmail.com",
+      password: "123456",
+    };
+
+    const { status, body } = (await request(app).post("/api/auth/login")).send(
+      loginData
+    );
+    expect(status).toBe(201);
+
+    expect(body.email).toBe(loginData.email);
+  });
+});
